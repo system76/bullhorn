7893a3c2f60371d2d811572f9e79ac60771d6e32
Sean Callan <sean@system76.com>
2021-03-04 10:13:27 -0700

    Setup Broadway for RabbitMQ

--- mix.exs
+++ mix.exs
@@ -30,8 +30,8 @@ defmodule Bullhorn.MixProject do
     [
       {:appsignal, "~> 1.0"},
       {:bamboo, "~> 1.6"},
-      {:bottle, github: "system76/bottle", ref: "041d8ae"},
-      {:broadway_sqs, "~> 0.6.0"},
+      {:bottle, github: "system76/bottle", ref: "1621c66"},
+      {:broadway_rabbitmq, "~> 0.6.5"},
       {:credo, "~> 1.3", only: [:dev, :test]},
       {:decorator, "~> 1.2"},
       {:ex_twilio, "~> 0.8.2"},
