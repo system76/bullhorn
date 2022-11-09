3e1d2f47238c14bcab6b919d3f3545d0cbf8e9d5
Andrew Hebert <andrew@system76.com>
2022-11-09 14:24:57 -0500

    format

--- config/dev.exs
+++ config/dev.exs
@@ -1,16 +1,15 @@
 import Config
 
-config :logger, :console,
-  level: :debug
+config :logger, :console, level: :debug
 
 config :bullhorn,
-       producer:
-         {BroadwayRabbitMQ.Producer,
-         queue: "bullhorn",
-         on_failure: :reject_and_requeue,
-         connection: [
-           username: "bullhorn",
-           password: System.get_env("RMQ_PASSWORD"),
-           host: System.get_env("RMQ_HOST", "localhost"),
-           port: System.get_env("RMQ_PORT", "5672")
-         ]}
+  producer:
+    {BroadwayRabbitMQ.Producer,
+     queue: "bullhorn",
+     on_failure: :reject_and_requeue,
+     connection: [
+       username: "bullhorn",
+       password: System.get_env("RMQ_PASSWORD"),
+       host: System.get_env("RMQ_HOST", "localhost"),
+       port: System.get_env("RMQ_PORT", "5672")
+     ]}
