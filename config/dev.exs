d6e14bf0cc602b90d29111238f3f4c8181920cdf
Andrew Hebert <andrew@system76.com>
2022-11-09 13:51:17 -0500

    readme note on related services

--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,16 @@
 import Config
+
+config :logger, :console,
+  level: :debug
+
+config :bullhorn,
+       producer:
+         {BroadwayRabbitMQ.Producer,
+         queue: "bullhorn",
+         on_failure: :reject_and_requeue,
+         connection: [
+           username: "bullhorn",
+           password: System.get_env("RMQ_PASSWORD"),
+           host: System.get_env("RMQ_HOST", "localhost"),
+           port: System.get_env("RMQ_PORT", "5672")
+         ]}
