2b7932364410d06f22a536bbda9e5a30eb067eeb
Andrew Hebert <andrew@system76.com>
2023-02-10 14:11:14 -0500

    support for processing attachments
    in particular, `html-pdf` type for converting html as pdf

--- test/bullhorn/templates_test.exs
+++ test/bullhorn/templates_test.exs
@@ -3,7 +3,10 @@ defmodule Bullhorn.TemplatesTest do
   use Bamboo.Test
 
   alias Bottle.Templates.V1.TemplatedEmail
+  alias Bottle.Templates.V1.TypedAttachment
   alias Bullhorn.Email.Templates
+
+  @moduletag capture_log: true
 
   describe "valid send_email/1" do
     {:ok, _} =
@@ -20,6 +23,22 @@ defmodule Bullhorn.TemplatesTest do
     assert_email_delivered_with(subject: "test")
   end
 
+  describe "valid send_email/1 with attachment" do
+    {:ok, _} =
+      Templates.send_email(
+        TemplatedEmail.new(
+          template_name: "template1",
+          form_variables: %{a: "1", b: "2"},
+          email_from: "test@example.com",
+          email_to: "user@example.com",
+          subject: "test",
+          attachments: [TypedAttachment.new(type: "html-pdf", source: "<html><body><p>test</p></body></html>", file_name: "test.pdf")]
+        )
+      )
+
+    assert_email_delivered_with(subject: "test")
+  end
+
   describe "invalid send_email/1" do
     {:error, _} =
       Templates.send_email(
@@ -28,7 +47,7 @@ defmodule Bullhorn.TemplatesTest do
           form_variables: 123,
           email_from: "test@example.com",
           email_to: "user@example.com",
-          subject: "test fail"
+          subject: "failure expected"
         )
       )
 
