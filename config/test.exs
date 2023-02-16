3f6317a5f41bdf35bdd1b472a09e49e3f9fe4ec8
Andrew Hebert <andrew@system76.com>
2023-02-16 11:41:19 -0500

    format

--- config/test.exs
+++ config/test.exs
@@ -6,4 +6,4 @@ config :bullhorn,
 config :bullhorn, Bullhorn.Mailer, adapter: Bamboo.TestAdapter
 
 config :logger,
-       level: System.get_env("LOG_LEVEL", "critical") |> String.to_existing_atom()
+  level: System.get_env("LOG_LEVEL", "critical") |> String.to_existing_atom()
