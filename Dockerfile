89e303d903e7515f4e40be8b74dc3749f8ae66f2
Yongjin Chong <zephyranthes03@gmail.com>
2025-02-10 13:15:45 -0700

    Update bottle version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM elixir:1.18.2-slim as build
+FROM elixir:1.17.3 as build
 
 # Install deps
 RUN set -xe; \
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bullhorn.MixProject do
     [
       app: :bullhorn,
       version: "0.1.0",
-      elixir: "~> 1.18.2",
+      elixir: "~> 1.17.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [
